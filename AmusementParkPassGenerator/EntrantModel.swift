be91aa96847dbd7ac1293636f9675539d6941b1e
Christopher Bonuel <bonuelc@gmail.com>
2016-07-15 13:57:53 -0600

    Add AreaAccessible Protocols

--- AmusementParkPassGenerator/EntrantModel.swift
+++ AmusementParkPassGenerator/EntrantModel.swift
@@ -6,3 +6,42 @@
 //  Copyright © 2016 Christopher Bonuel. All rights reserved.
 //
 
+
+// MARK: Can-Do Protocols
+
+// MARK: Area Accessible Protocols
+
+protocol AreaAccessible {
+}
+
+protocol AmusementAreaAccessible: AreaAccessible {
+}
+
+protocol KitchenAreaAccessible: AreaAccessible {
+}
+
+protocol RideControlAreaAccessible: AreaAccessible {
+}
+
+protocol MaintenanceAreaAccessible: AreaAccessible {
+}
+
+protocol OfficeAreaAccessible: AreaAccessible {
+}
+
+// MARK: Is-A Protocols
+
+protocol EntrantType {
+}
+
+protocol GuestType: EntrantType {
+}
+
+protocol ManagerType: EntrantType {
+}
+
+protocol EmployeeType: EntrantType {
+}
+
+protocol VendorType: EntrantType {
+}
