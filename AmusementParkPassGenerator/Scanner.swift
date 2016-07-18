c023b1794a665a1ae4a14818681c1382271eb980
Christopher Bonuel <bonuelc@gmail.com>
2016-07-18 14:23:33 -0600

    Add Scanner class with static func scan()

--- AmusementParkPassGenerator/Scanner.swift
+++ AmusementParkPassGenerator/Scanner.swift
@@ -14,3 +14,31 @@ enum AccessType: String {
     // Discount Access
     case FoodDiscount, MerchandiseDiscount
 }
+
+class Scanner {
+    static func scan(entrant: EntrantType, accessType: AccessType) -> Bool {
+        switch accessType {
+        // Area Access
+        case .AmusementAreas:
+            return entrant is AmusementAreaAccessible
+        case .KitchenAreas:
+            return entrant is KitchenAreaAccessible
+        case .RideControlAreas:
+            return entrant is RideControlAreaAccessible
+        case .MaintenanceAreas:
+            return entrant is MaintenanceAreaAccessible
+        case .OfficeAreas:
+            return entrant is OfficeAreaAccessible
+        // Ride Access
+        case .AllRides:
+            return entrant is AllRidesAcesssible
+        case .SkipRideLines:
+            return entrant is SkipAllRideLinesAcessible
+        // Discount Access
+        case .FoodDiscount:
+            return entrant is FoodDiscountAccessible
+        case .MerchandiseDiscount:
+            return entrant is MerchandiseDiscountAccessible
+        }
+    }
+}
