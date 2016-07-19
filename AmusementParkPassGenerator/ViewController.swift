57b997536dfac2d4616fd5a3f6a65c15ce59e6b4
Christopher Bonuel <bonuelc@gmail.com>
2016-07-18 20:31:33 -0600

    Implement createValidEntrants() to create test data

--- AmusementParkPassGenerator/ViewController.swift
+++ AmusementParkPassGenerator/ViewController.swift
@@ -34,6 +34,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        createValidEntrants()
     }
 
     override func didReceiveMemoryWarning() {
@@ -61,5 +62,33 @@ class ViewController: UIViewController {
     func playSound(url: NSURL) {
         AudioServicesCreateSystemSoundID(url, &sound)
         AudioServicesPlaySystemSound(sound)
+    }
+    
+    func createValidEntrants() {
+        entrants.append(ClassicGuest())
+        
+        entrants.append(VIPGuest())
+        
+        try! entrants.append(FreeChildGuest(birthMonth: 7, birthDay: 19, birthYear: 2015))
+        
+        let marioName = try! FullName(firstName: "Mario", lastName: "Mario")
+        let marioAddress = try! FullAddress(streetAddress: "123 Mushroom", city: "Green Pipe", state: "MM", zipCode: 12345)
+        let Mario = HourlyEmployeeFoodServices(fullName: marioName, fullAddress: marioAddress)
+        entrants.append(Mario)
+        
+        let yoshiName = try! FullName(firstName: "Yoshi", lastName: "Horse")
+        let yoshiAddress = marioAddress
+        let Yoshi = HourlyEmployeeRideServices(fullName: yoshiName, fullAddress: yoshiAddress)
+        entrants.append(Yoshi)
+        
+        let luigiName = try! FullName(firstName: "Luigi", lastName: "Mario")
+        let luigiAddress = marioAddress
+        let Luigi = HourlyEmployeeMaintenance(fullName: luigiName, fullAddress: luigiAddress)
+        entrants.append(Luigi)
+        
+        let bowserName = try! FullName(firstName: "Bowser", lastName: "Reptile")
+        let bowserAddress = try! FullAddress(streetAddress: "123 Lava", city: "Phoenix", state: "AZ", zipCode: 12345)
+        let Bowser = Manager(fullName: bowserName, fullAddress: bowserAddress)
+        entrants.append(Bowser)
     }
 }
