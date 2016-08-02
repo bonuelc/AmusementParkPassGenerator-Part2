31c59822b1db860155045f051dcd4f07745a6e8c
Christopher Bonuel <bonuelc@gmail.com>
2016-08-02 15:07:18 -0600

    Add a singleton DummyEntrantFactory class
    The generated dummy entrants will be used by
    EntrantPassVC.entrantSubtypeTapped() to figure out what protocols each
    entrant conforms to.

---
+++ AmusementParkPassGenerator/DummyEntrantFactory.swift
@@ -0,0 +1,14 @@
+//
+//  DummyEntrantFactory.swift
+//  AmusementParkPassGenerator
+//
+//  Created by Christopher Bonuel on 8/2/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+class DummyEntrantFactory {
+    
+    static let sharedInstance = DummyEntrantFactory()
+    
+    private init() {}
+}
