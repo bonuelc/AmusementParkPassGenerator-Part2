088645aa171674283b38384df7922eabf8a8dd8a
Christopher Bonuel <bonuelc@gmail.com>
2016-08-03 10:46:15 -0600

    Implement entrant enums for DummyEntrantFactory
    The factory will have functions that will take these enums then return
    a dummy entrant for EntrantPassViewController.entrantSubtypeTapped()
    to pass on to disableUnrequiredFieldsForEntrant().

--- AmusementParkPassGenerator/DummyEntrantFactory.swift
+++ AmusementParkPassGenerator/DummyEntrantFactory.swift
@@ -6,6 +6,27 @@
 //  Copyright © 2016 Christopher Bonuel. All rights reserved.
 //
 
+enum Guest {
+    case Classic, VIP, SeasonPass, FreeChild, Senior
+}
+
+enum Employee {
+    case FoodServices
+    case RideServices
+    case Maintenance
+    case Manager
+}
+
+enum ContractorProjectNumber: Int {
+    case P1001 = 1001, P1002, P1003
+    case P2001 = 2001, P2002
+}
+
+enum VendorName: String {
+    case Acme, Orkin, Fedex
+    case NWElectrical = "NW Electrical"
+}
+
 class DummyEntrantFactory {
     
     static let sharedInstance = DummyEntrantFactory()
