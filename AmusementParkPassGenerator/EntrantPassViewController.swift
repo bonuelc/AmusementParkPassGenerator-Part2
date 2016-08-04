7550a8efee2cbf25c827a7623508a8ae1c3899a2
Christopher Bonuel <bonuelc@gmail.com>
2016-08-04 15:00:50 -0600

    Add/hook up EntrantPassViewController.swift

---
+++ AmusementParkPassGenerator/EntrantPassViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EntrantPassViewController.swift
+//  AmusementParkPassGenerator
+//
+//  Created by Christopher Bonuel on 8/4/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class EntrantPassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
